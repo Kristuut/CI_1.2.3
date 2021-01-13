fd69bf75193227fa9e23601a0d3b059741595fa5
Kristina Sachenko <mytino4ka@mail.ru>
2021-01-13 17:19:48 +0700

    Initial commit

---
+++ src/test/java/Test1.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+import io.restassured.http.ContentType;
+import static io.restassured.RestAssured.given;
+
+class Test1 {
+    @Test
+    void shouldReturnDemoAccounts() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+        //.body(/* --> ваша проверка здесь <-- */)
+        ;
+    }
+}
