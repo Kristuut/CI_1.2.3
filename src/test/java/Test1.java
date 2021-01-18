934eacddcb87c7cad1319dd4b33987d9dba2b3b4
Kristina Sachenko <mytino4ka@mail.ru>
2021-01-18 15:52:48 +0700

    Изменен тест

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,19 +1,23 @@
+import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.Test;
 import io.restassured.http.ContentType;
 import static io.restassured.RestAssured.given;
+import org.junit.jupiter.api.Test;
 
-class Test1 {
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class Test1 {
     @Test
-    void shouldReturnDemoAccounts() {
+    void shouldPostBodyRequest() {
         given()
                 .baseUri("https://postman-echo.com")
                 .body("some data")
                 .when()
                 .post("/post")
-
                 .then()
                 .statusCode(200)
-       //.body(/* --> ваша проверка здесь <-- */)
-        ;
+                .body("data", equalTo("some data"));
+
     }
 }
